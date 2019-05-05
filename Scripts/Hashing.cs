d859c33389fc1fdae19c24c3c64904288bcd0ca9
caxapexac <caxapexac@gmail.com>
2019-05-05 18:19:42 +0300

    Added some scripts

--- Scripts/Hashing.cs
+++ Scripts/Hashing.cs
@@ -45,5 +45,17 @@
 
             return hash;
         }
+		
+		private static string CalculateMd5(string filename)
+		{
+			using (var md5 = MD5.Create())
+			{
+				using (var stream = File.OpenRead(filename))
+				{
+					var hash = md5.ComputeHash(stream);
+					return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+				}
+			}
+		}
     }
 }
